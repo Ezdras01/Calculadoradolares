f16c79ec2ae1b186be8e00498ea5a46ec3c08d76
lehi170993 <lehi170993@gmail.com>
2024-03-26 19:08:28 +0000

    Se agrega la conversion de dolar a peso

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import './screens/inicio.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Inicio(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculadora de Divisas',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +32,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Convertidor de Divisas'),
     );
   }
 }
---
+++ lib/screens/inicio.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Inicio extends StatefulWidget {
+  const Inicio({Key? key}) : super(key: key);
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  final pesoMexicano = TextEditingController();
+  final dolares = TextEditingController();
+
+  final test = TextEditingController();
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    pesoMexicano.dispose();
+    dolares.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          AppBar(
+            title: Text('Convertidor de divisas'),
+          ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Text("Introduce el valor del dollar"),
+            ),
+          ),
+          Center(
+              child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Dollar',
+                ),
+                controller: dolares,
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text("Introduce la cantidad a convertir"),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Peso Mexicano',
+                ),
+                controller: pesoMexicano,
+              ),
+              Center(
+                child: Column(children: [
+                  Padding(
+                      padding: EdgeInsets.all(20),
+                      child: ElevatedButton(
+                        onPressed: () {
+
+                          var dolares_input = double.parse(this.dolares.text);
+                          var pesoMexicano_input = double.parse(this.pesoMexicano.text);
+
+                          var total = pesoMexicano_input / dolares_input;
+
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                // Retrieve the text the that user has entered by using the
+                                // TextEditingController.
+                                content: Text("Dolar ${dolares.text} Peso m${pesoMexicano.text} \n Total:${total.toStringAsFixed(2)}"),
+                              );
+                            },
+                          );
+                        },
+                        child: Text('Convertir'),
+                      )),
+                ]),
+              ),
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
